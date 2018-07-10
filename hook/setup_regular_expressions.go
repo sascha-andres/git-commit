6b4cce5f3f1743075654464724598dc7c49e912a
Sascha Andres <sascha.andres@outlook.com>
2018-07-10 15:00:00 +0200

    feat: make program quiet

--- hook/setup_regular_expressions.go
+++ hook/setup_regular_expressions.go
@@ -1,13 +1,10 @@
 package hook
 
 import (
-	"fmt"
 	"regexp"
 )
 
 func (cfg *Configuration) setupRegularExpressions() error {
-	fmt.Println("info: setup regular expressions")
-	fmt.Println("info: setup ignore regular expressions")
 	if cfg.IgnoreExpressions != nil && len(cfg.IgnoreExpressions) > 0 {
 		cfg.ignoreCompiled = make([]*regexp.Regexp, 0)
 		for _, expr := range cfg.IgnoreExpressions {
@@ -18,7 +15,6 @@ func (cfg *Configuration) setupRegularExpressions() error {
 			cfg.ignoreCompiled = append(cfg.ignoreCompiled, r)
 		}
 	}
-	fmt.Println("info: setup subject regular expressions")
 	if cfg.FirstLineExpressions != nil && len(cfg.FirstLineExpressions) > 0 {
 		cfg.firstLineCompiled = make([]*regexp.Regexp, 0)
 		for _, expr := range cfg.FirstLineExpressions {
@@ -29,7 +25,6 @@ func (cfg *Configuration) setupRegularExpressions() error {
 			cfg.firstLineCompiled = append(cfg.firstLineCompiled, r)
 		}
 	}
-	fmt.Println("info: setup occurs regular expressions")
 	if cfg.FindOccurrenceExpressions != nil && len(cfg.FindOccurrenceExpressions) > 0 {
 		cfg.occursCompiled = make([]*regexp.Regexp, 0)
 		for _, expr := range cfg.FindOccurrenceExpressions {
