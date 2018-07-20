f308346f34c4cb4e1240c8a93f4784b46719d7ff
Sascha Andres <sascha.andres@outlook.com>
2018-07-20 14:07:56 +0200

    refactor: cleanup return statements
    reduce number of return statements in some methods

--- cmd/main.go
+++ cmd/main.go
@@ -15,49 +15,11 @@ import (
 func main() {
 	methods.PrintVersion()
 
-	if len(os.Args) < 2 {
-		methods.Help()
-		os.Exit(0)
+	if prepare() {
 		return
 	}
 
-	if os.Args[1] == "install" {
-		os.Exit(methods.InstallHook())
-		return
-	}
-
-	if os.Args[1] == "uninstall" {
-		os.Exit(methods.UninstallHook())
-		return
-	}
-
-	validationResult := validateInput()
-	if 0 != validationResult {
-		os.Exit(validationResult)
-		return
-	}
-
-	commitMessageFile := os.Args[1]
-	config, err := hook.NewForVersion(commitMessageFile)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-		return
-	}
-
-	commitFileContent, err := loadCommitMessageFile(commitMessageFile)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-		return
-	}
-
-	ok, err := config.Validate(commitFileContent)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-		return
-	}
+	ok := validate()
 
 	if !ok {
 		os.Exit(1)
@@ -65,6 +27,70 @@ func main() {
 	}
 
 	os.Exit(0)
+}
+
+// prepare checks input and runs actions for install/uninstall when requested
+func prepare() bool {
+	if len(os.Args) < 2 {
+		methods.Help()
+		os.Exit(0)
+		return true
+	}
+
+	var (
+		fn  func() int
+		run bool
+	)
+
+	if os.Args[1] == "install" {
+		fn = methods.InstallHook
+		run = true
+	}
+
+	if os.Args[1] == "uninstall" {
+		fn = methods.UninstallHook
+		run = true
+	}
+
+	if run {
+		os.Exit(fn())
+		return true
+	}
+
+	validationResult := validateInput()
+	if 0 != validationResult {
+		os.Exit(validationResult)
+		return true
+	}
+
+	return false
+}
+
+// validate runs validation
+func validate() bool {
+	commitMessageFile := os.Args[1]
+	config, err := hook.NewForVersion(commitMessageFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+		return false
+	}
+
+	commitFileContent, err := loadCommitMessageFile(commitMessageFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+		return false
+	}
+
+	ok, err := config.Validate(commitFileContent)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+		return false
+	}
+
+	return ok
 }
 
 // validateInput checks program oarameters when running
