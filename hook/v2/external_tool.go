d1bf451458094d207f8681f785e707e1d49b6f2a
Sascha Andres <sascha.andres@outlook.com>
2018-07-20 10:55:44 +0200

    refactor: cleanup external tool calls
    split functions

--- hook/v2/external_tool.go
+++ hook/v2/external_tool.go
@@ -3,6 +3,7 @@ package v2
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"syscall"
 )
@@ -11,42 +12,57 @@ import (
 func (cfg *Configuration) runExternalTools() bool {
 	result := true
 	for _, val := range cfg.Externals {
-		cmd := exec.Command(val.Command[0], val.Command[1:]...)
-
-		var stdout, stderr bytes.Buffer
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-
-		if err := cmd.Start(); err != nil {
-			fmt.Println(fmt.Sprintf("error: unable to start [%s]: %s", val.Name, err.Error()))
-			result = false
-		} else {
-			if err := cmd.Wait(); err != nil {
-				result = val.handleExecuteError(stdout, stderr, err) && result
-			}
-		}
+		result = val.execute() && result
 	}
 	return result
 }
 
+// execute runs command
+func (val *Tool) execute() bool {
+	cmd := exec.Command(val.Command[0], val.Command[1:]...)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println(fmt.Sprintf("error: unable to start [%s]: %s", val.Name, err.Error()))
+		return false
+	}
+	if err := cmd.Wait(); err != nil {
+		return val.handleExecuteError(stdout, stderr, err)
+	}
+	return true
+}
+
 // handleExecuteError does the lifting for an execute error
 func (val *Tool) handleExecuteError(stdout, stderr bytes.Buffer, err error) bool {
-	if exitError, ok := err.(*exec.ExitError); ok {
-		fmt.Println("output:")
-		fmt.Println()
-		fmt.Println(stdout.String())
+	var (
+		exitError *exec.ExitError
+		ok        bool
+	)
 
-		fmt.Println("error:")
-		fmt.Println()
-		fmt.Println(stderr.String())
-		if _, ok := exitError.Sys().(syscall.WaitStatus); ok {
-			fmt.Println(fmt.Sprintf("%s: execution of [%s] failed", val.Severity, val.Name))
-			if val.Severity != ErrorSeverity {
-				return true
-			}
+	if exitError, ok = err.(*exec.ExitError); !ok {
+		fmt.Println(fmt.Sprintf("error: unable get exit code [%s]: %s", val.Name, err.Error()))
+	}
+	printOutput(stdout, stderr)
+
+	if _, ok := exitError.Sys().(syscall.WaitStatus); ok {
+		fmt.Println(fmt.Sprintf("%s: execution of [%s] failed", val.Severity, val.Name))
+		if val.Severity != ErrorSeverity {
+			return true
 		}
-	} else {
-		fmt.Println(fmt.Sprintf("error: unable to start [%s]: %s", val.Name, err.Error()))
 	}
 	return false
+}
+
+// printOutput dumps command output
+func printOutput(stdout, stderr bytes.Buffer) {
+	fmt.Println("output:")
+	fmt.Println()
+	fmt.Println(stdout.String())
+
+	fmt.Println("error:")
+	fmt.Println()
+	fmt.Fprintln(os.Stderr, stderr.String())
 }
